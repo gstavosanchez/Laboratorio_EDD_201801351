f4a3a91dd1e11c7f6b4b17f061caf56d79cd99ca
Marvin <marvin1ronal@gmail.com>
2020-08-28 00:07:19 -0600

    Calificacion3

--- Tarea3/main.cpp
+++ Tarea3/main.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<stdlib.h>
-#include<conio.h>
+
 #include "Arbol.h"
 using namespace std;
 
@@ -12,17 +12,25 @@ int main(){
     Arbol* arbolBinario = new Arbol();
 
 
-    arbolBinario->addRecursivo(arbolBinario->getRoot(),8);
-    arbolBinario->addRecursivo(arbolBinario->getRoot(),4);
     arbolBinario->addRecursivo(arbolBinario->getRoot(),10);
-    arbolBinario->addRecursivo(arbolBinario->getRoot(),12);
     arbolBinario->addRecursivo(arbolBinario->getRoot(),1);
-    
+    arbolBinario->addRecursivo(arbolBinario->getRoot(),20);
+    arbolBinario->addRecursivo(arbolBinario->getRoot(),15);
+    arbolBinario->addRecursivo(arbolBinario->getRoot(),8);
+    arbolBinario->addRecursivo(arbolBinario->getRoot(),96);
+    arbolBinario->addRecursivo(arbolBinario->getRoot(),23);
+    arbolBinario->addRecursivo(arbolBinario->getRoot(),4);
+    arbolBinario->addRecursivo(arbolBinario->getRoot(),7);
+    arbolBinario->addRecursivo(arbolBinario->getRoot(),32);
 
     arbolBinario->postOrden(arbolBinario->getRoot());
+    cout<<"\n";
+    arbolBinario->inOrden(arbolBinario->getRoot());
+    cout<<"\n";
+    arbolBinario->preOrden(arbolBinario->getRoot());
 
 
-    getch();
+    
     return 0 ;
 }
 
